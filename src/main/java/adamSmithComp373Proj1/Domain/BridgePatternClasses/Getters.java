839fca23dc1396611e8e0b63ab3f648de69fac97
Adam Smith <asmith6@luc.edu>
2021-04-26 16:19:58 -0500

    Skeletoning Bridge Pattern

---
+++ src/main/java/adamSmithComp373Proj1/Domain/BridgePatternClasses/Getters.java
@@ -0,0 +1,14 @@
+
+public abstract class Getters(){
+    
+ public String getName();
+
+ public String getLocation();
+
+ public String getPhone();
+
+ public Integer getNumberOfRooms();
+
+ public Integer getOccupiedRooms();
+
+}
