e6e13af82e85b459a7438987e6c8cd18ef12b959
nehapatel083 <43070434+nehapatel083@users.noreply.github.com>
2021-04-26 21:43:27 -0500

    added observer classes.

---
+++ ObserverPattern/Observers/FacilityObserver.java
@@ -0,0 +1,8 @@
+package com.observer.observers;
+
+/*
+FacilityObserver is the interface used to update observers with changes to FacilitySubject.
+ */
+public interface FacilityObserver {
+    public void update();
+}
