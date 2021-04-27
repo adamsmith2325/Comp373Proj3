4a888aba8fbec956cb7e7b462e580f31a37865c9
nehapatel083 <43070434+nehapatel083@users.noreply.github.com>
2021-04-26 22:04:54 -0500

    updated inspection

--- ObserverPattern/Subject/Inspection.java
+++ ObserverPattern/Subject/Inspection.java
@@ -6,6 +6,61 @@ import com.observer.observers.FacilityObserver;
 
 public enum Type{TYPE, TYPE2, TYPE3}
 
-public class Inspection implements FacilityGetSetInterface {
+public class Inspection implements FacilityGetSetInterface  {
 
+    private InspectionDAO Conn = new InspectionDAO();
+    private Integer InspectionID;
+    private String Name = Conn.getName();
+    private Type InsType = Conn.getType();
+    private String Details = Conn.getDetails();
+    private Integer facility = Conn.getFacility();
+    private Integer Room = Conn.getRoom();
+
+    public Inspection(Integer ID) {
+        this.InspectionID = ID;
+    }
+
+    public int getInspectionID() {
+        return Conn.getInspectionID(this.InspectionID);
+    }
+    public void addInspectionID(int inspectionID) {
+        Conn.addInspectionID(this.InspectionID);
+    }
+
+    public String getName() {
+        return this.Name;
+    }
+    public Type getType() {
+        return this.InsType;
+    }
+    public String getDetails() {
+        return this.Details;
+    }
+    public int getFacility() {
+        return Conn.getFacility(this.facility);
+    }
+
+    public Integer getRoom(){
+        return Conn.getRoom(this.Room);
+    }
+    public void setRoom(int InspectionID){
+        Conn.setRoom(this.InspectionID);
+    }
+
+    public void setFacility(Facility facility) {
+        Conn.setFacility();
+    }
+
+    public void setFacility(Integer FacilityID) {
+
+        Conn.setFacility();
+
+    }
+
+    public void setType(String InsType) {
+        Conn.setType(this.InspectionID);
+    }
+    public void setDetails(String insDetails) {
+        Conn.setDetails();
+    }
 }
