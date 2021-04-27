69c95876c6dd5a0ff16976209dd8f452b15c3d8b
nehapatel083 <43070434+nehapatel083@users.noreply.github.com>
2021-04-26 21:20:00 -0500

    going to make a folder in main

--- src/main/java/adamSmithComp373Proj1/Domain/ObserverPatternClasses/Observerp.java
+++
@@ -1,9 +0,0 @@
-package com.design.observer;
-//interface that defines the behaviors of the subscriber
-public interface Observer {
-    //method will allow observer to get latest message
-    public void update();
-    // method will allow the observer to subscribe to a subject
-    public void setSubject(Subject subject);
-
-}
