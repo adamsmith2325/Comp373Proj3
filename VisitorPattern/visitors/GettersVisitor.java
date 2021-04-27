e60824710fd5db78da06a41a19e8afe4c0597002
Adam Smith <asmith6@luc.edu>
2021-04-26 23:00:43 -0500

    Visitors Made

---
+++ VisitorPattern/visitors/GettersVisitor.java
@@ -0,0 +1,10 @@
+package adamSmithComp373Proj1.VisitorPattern.visitors;
+import adamSmithComp373Proj1.VisitorPattern.elements;
+
+public interface GettersVisitor {
+    public visit(FacilityLight facility);
+    public visit(InspecLight Inspection);
+    public visit(MainLight Maintainence);
+    public visit(UseLight Use);
+}
+
---
+++ VisitorPattern/visitors/Visitor.java
@@ -0,0 +1,9 @@
+package adamSmithComp373Proj1.VisitorPattern.visitors;
+import adamSmithComp373Proj1.VisitorPattern.elements;
+
+public interface Visitor {
+    public visit(FacilityLight facility);
+    public visit(InspecLight Inspection);
+    public visit(MainLight Maintainence);
+    public visit(UseLight Use);
+}
