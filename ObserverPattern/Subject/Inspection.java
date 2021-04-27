c38c62acb71abec1df7ce1228bf9e82d832cbce9
nehapatel083 <43070434+nehapatel083@users.noreply.github.com>
2021-04-26 22:04:34 -0500

    added subject

---
+++ ObserverPattern/Subject/Inspection.java
@@ -0,0 +1,11 @@
+
+package com.observer.subject;
+
+import com.observer.observers.FacilityObserver;
+
+
+public enum Type{TYPE, TYPE2, TYPE3}
+
+public class Inspection implements FacilityGetSetInterface {
+
+}
